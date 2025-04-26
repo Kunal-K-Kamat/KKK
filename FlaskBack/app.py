ef915aaaa454eb3050e92b6abdfaa84407b92db2
Kunal-K-Kamat <kunalkamat24@gmail.com>
2025-04-26 08:28:06 +0530

    app.py

--- FlaskBack/app.py
+++ FlaskBack/app.py
@@ -263,11 +263,25 @@ class FeatureExtraction:
     
     def __init__(self,url):
         self.url = url
-        self.domain = ""
-        self.whois_response = ""
-        self.urlparse = ""
-        self.response = ""
-        self.soup = ""
+        try:
+            self.response = requests.get(url, timeout=5)
+            self.soup = BeautifulSoup(self.response.text, 'html.parser')
+        except:
+            self.response = None
+            self.soup = None
+
+        try:
+            self.urlparse = urlparse(url)
+            self.domain = self.urlparse.netloc
+        except:
+            self.urlparse = None
+            self.domain = ""
+
+        try:
+            self.whois_response = whois.whois(self.domain)
+        except:
+            self.whois_response = None
+
 
         self.features = [
             self.UsingIp(),
